31d271507befa6c798ed77ea458101e815ee878c
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2020-03-17 13:46:59 +0100

    fix

--- create-items.sh
+++ create-items.sh
@@ -2,6 +2,7 @@
 set -x -e
 NOW=$(date +%Y%m%d)
 zip=numworx-items-$NOW.zip
-java -jar target/taoexport.jar target/$zip
+cd target
+java -jar taoexport.jar $zip
 V=/volumes/FI-Sites/www-dev/dwo/apps/
-cp target/$zip $V
+cp $zip $V
