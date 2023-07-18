4c4a8c87f87562f7ef0f6c7f71facb52cddf6906
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2023-07-18 09:50:13 +0200

    aaarchhh

--- create-items.sh
+++ create-items.sh
@@ -3,6 +3,6 @@ set -x -e
 NOW=$(date +%Y%m%d)
 zip=numworx-items-$NOW.zip
 cd target
-java -jar taoexport.jar -Dtaoexport.user=$USER -Dtaoexport.pass=$PASS $zip
+java -Dtaoexport.user=$USER -Dtaoexport.pass=$PASS -jar taoexport.jar  $zip
 V=/Volumes/FI-Sites/www-dev/dwo/apps/
 cp $zip $V
