858d4c4d2fae9947b5badea22a019f910c4eb0aa
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2024-03-18 13:40:20 +0100

    met scp ip cp

--- create-items.sh
+++ create-items.sh
@@ -3,6 +3,6 @@ set -x -e
 NOW=$(date +%Y%m%d)
 zip=numworx-items-$NOW.zip
 cd target
-java -Dtaoexport.user=$USER -Dtaoexport.pass=$PASS -jar taoexport.jar  $zip
-V=/Volumes/FI-Sites/www-dev/dwo/apps/
-cp $zip $V
+java -Dtaoexport.user=$DWOUSER -Dtaoexport.pass=$DWOPASS -jar taoexport.jar  $zip
+V=$USER@gemini.science.uu.nl:/science/wwwprojects/FI-Sites/www-dev/dwo/apps/
+scp $zip $V
